8efd6bcf16a253a1a1d35ef531299a1a7de1b64a
Raisahab77 <61798248+Raisahab77@users.noreply.github.com>
2021-07-02 08:37:52 +0530

    Add files via upload

--- home.py
+++ home.py
@@ -1,139 +1,251 @@
-import pyttsx3
-from datetime import *
-import speech_recognition as sr
-import requests
-import wikipedia
-import webbrowser
-import os
-import time
-import calculator
-import speedtest
-import platform
-import pyautogui
-import psutil
+try:
+    import pyttsx3
+    from datetime import *
+    # this module detect human voice and converts it into text form.
+    import speech_recognition as sr
+    # this module is use to request on server.
+    import requests
+    # This module use to fetch data from wikipedia
+    import wikipedia
+    # This module is use to open web browser
+    import webbrowser
+    # This module is use to work on system os
+    import os
+    import time
+    import calculator
+    # This module is used for Test internet speed
+    import speedtest   #pip install speedtest-cli
+    # This module tells us that which os we are using basically it tells all the information about our os
+    import platform
+    # This module is use to automate tasks
+    import pyautogui
+    import psutil
+    import pyjokes
+    from googlesearch import search
+    import tkinter as tk
+    from PIL import Image, ImageTk
+except Exception as e:
+    print(e)
 ############################################ Weather API #########################################################
 
 # newsUrl = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=9f17ea0361774831bb8906e2426c1e22'
 api_address = "http://api.openweathermap.org/data/2.5/weather?appid=40b98f754f6b23dc8725fd2cfc6d9769&q="
 
+def currentDay ():
+    """
+    This function tells current day.
+    """
+    try:
+        today = date.today().strftime("%A")
+        speak(f"Today's date is : {today}")
+    except Exception as e:
+        print(e)
+    
+def currentDate():
+    """
+    This function tells current date.
+    """
+    try:
+        date1 = date.today()
+        speak(date1)
+    except Exception as e:
+        print(e)
 
-def batterPercantage():
-    battery = psutil.sensors_battery()
-    if battery.percent == 100:
-        speak("battery is fully charged")
-    else:
-        speak(f"battery is {battery.percent} percent charge") 
-    if battery.power_plugged == 'true':
-        speak("battery is charging")
-    else:
-        speak("battery is not charging")          
+def jokes():
+    """
+    This function tells joke 
+    using pyjoke module.
+    """
+    try:
+        My_joke = pyjokes.get_joke(language="en", category="all")  
+        speak(My_joke)
+        query=take_command().lower()
+        if "haha " in query or "good one" in query:
+            speak("thanks sir should i tell you one more joke")
+            query = take_command().lower()
+            if "yeah" in query:
+                jokes()
+            else :
+                return  
+    except Exception as e:
+        print(e)
+
+def batteryPercantage():
+    """
+    This function uses psutil
+    module to get battery percantage
+    """
+    try:
+        battery = psutil.sensors_battery()
+        if battery.percent == 100:
+            speak("battery is fully charged")
+        else:
+            speak(f"battery is {battery.percent} percent charge") 
+        if battery.power_plugged == 'true':
+            speak("battery is charging")
+        else:
+            speak("battery is not charging")          
+    except Exception as e:
+        print(e)
 
 def fullSystemDetail():
     pass
 
-
 def osName ():
-    os = platform.system()
-    speak(f"you are using {os} operating system")
-
+    """
+    This function tells that which 
+    operating system  we are using now.
+    """
+    try:
+        os = platform.system()
+        speak(f"you are using {os} operating system")
+    except Exception as e:
+        print(e)
 
 def screenshot():
-    myScreenshot = pyautogui.screenshot()
-    speak("taking screenshot")
-    lst = os.listdir('jarvis\screenshot')
-    lstLength = int(len(lst))
-    path = f"jarvis\screenshot\screenshot{lstLength+1}.png"
-    myScreenshot.save(path)
+    """
+    This function takes a screenshot
+    using pyautogui and save it in 
+    screenshot directory.
+    """
+    try:
 
+        isdir = os.path.isdir("screenshot")
+        if isdir==True:
+            myScreenshot = pyautogui.screenshot()
+            speak("taking screenshot")
+            lst = os.listdir('screenshot')
+            lstLength = int(len(lst))
+            path = f"screenshot\\screenshot{lstLength+1}.png"
+            myScreenshot.save(path)
+            speak("Screenshot taken")
+        else :
+            os.mkdir("screenshot")
+            screenshot()
+    except Exception as e:
+        print(e)
+        speak("Can not take screenshot")
 
 def speedTest():
-    speak("ok sir trying to fetch download speed")
-    st = speedtest.Speedtest()
-    downloadSpeed = st.download()
-    downloadSpeed = downloadSpeed/1048576   #for converting bytes in Kbytes we have to divied by 1024 and then To convert from Kbyte to Mbyte we have to divide by 1024 then we can multiply 1024*1024 = 1048576  
-    downloadSpeed = "{:.2f}".format(downloadSpeed)
-    speak(f"{downloadSpeed} Megabyte per second")
+    """
+    This function used to fetch
+    the internet speed.
+    """
+    try:
+        speak("ok sir trying to fetch download speed")
+        st = speedtest.Speedtest()
+        downloadSpeed = st.download()
+        downloadSpeed = downloadSpeed/1048576   #for converting bytes in Kbytes we have to divied by 1024 and then To convert from Kbyte to Mbyte we have to divide by 1024 then we can multiply 1024*1024 = 1048576  
+        downloadSpeed = "{:.2f}".format(downloadSpeed)
+        speak(f"{downloadSpeed} Megabyte per second")
+    except Exception as e:
+        print(e)
 
-
-def time ():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    speak(f"The time is {current_time}")
-
+def current_time ():
+    """
+    This function tells the 
+    current time using datetime module.
+    """
+    try:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        speak(f"The time is {current_time}")
+    except Exception as e:
+        print(e)
 
 def news():
-    newsUrl = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=9f17ea0361774831bb8906e2426c1e22'
-    json_data = requests.get(newsUrl).json()
-    for i in range(0, 10):
-        title = json_data['articles'][i]['title']
-        description = json_data['articles'][i]['description']
-        print(f"{i + 1} :-{title}")
-        speak(title)
-        print(f"        {description}")
-        speak(description)
-
+    """
+    This function use news api and fetch 
+    10 latest news and speak news.
+    """
+    try:
+        newsUrl = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=9f17ea0361774831bb8906e2426c1e22'
+        json_data = requests.get(newsUrl).json()
+        for i in range(0, 10):
+            title = json_data['articles'][i]['title']
+            description = json_data['articles'][i]['description']
+            print(f"{i + 1} :-{title}")
+            speak(title)
+            print(f"        {description}")
+            speak(description)
+    except Exception as e:
+        print(e)
 
 def weather_data():
     """
         This function use a API. This function send request to open weather site
         fetch data from that site a speak.
     """
-    url = api_address + "gorakhpur"  # in url we add a place address to get particular location weather data
+    try:
+        url = api_address + "gorakhpur"  # in url we add a place address to get particular location weather data
 
-    json_data = requests.get(url).json()
+        json_data = requests.get(url).json()
 
-    weather_data = json_data['weather'][0]['main']
-    temp_data = json_data['main']['temp']
-    temp_data = int(temp_data - 273.15)
-    # formatted_data = weather_data + temp_data
-    # print(json_data)
-    speak(f"Hello sir the weather is {weather_data} and the temerature is {temp_data} degree celsius")
-
-
+        weather_data = json_data['weather'][0]['main']
+        temp_data = json_data['main']['temp']
+        temp_data = int(temp_data - 273.15)
+        # formatted_data = weather_data + temp_data
+        # print(json_data)
+        speak(f"Hello sir the weather is {weather_data} and the temerature is {temp_data} degree celsius")
+    except Exception as e:
+        print(e)
 ################################################### Greet Function #############################################
 def greet():
     """
     This Function used for greeting
 
     """
-    hour = int(datetime.now().hour)
-    today = date.today()
-    curr_day = today.strftime("%B %d , %Y")
-    now = datetime.now()
-    curr_time = now.strftime("%H %M")
+    try:
+        hour = int(datetime.now().hour)
+        today = date.today()
+        curr_day = today.strftime("%B %d , %Y")
+        now = datetime.now()
+        curr_time = now.strftime("%H %M")
 
-    if hour >= 3 and hour <= 11:
-        speak(f"Hello sir good morning  It's {curr_day} and the time is {curr_time}")
-        weather_data()
-
-
-    elif hour >= 12 and hour <= 18:
-        speak("Hello Good After noon ")
+        if hour >= 3 and hour <= 11:
+            speak(f"Hello sir good morning  It's {curr_day} and the time is {curr_time}")
+            weather_data()
 
 
-    elif hour > 18 and hour <= 24:
-        speak("Hello Good evening ")
+        elif hour >= 12 and hour <= 18:
+            speak("Hello Good After noon ")
 
 
-    else:
-        speak("Hello Good night")
+        elif hour > 18 and hour <= 24:
+            speak("Hello Good evening ")
 
+
+        else:
+            speak("Hello Good night")
+    except Exception as e:
+        print(e)
 
 ################################################ Speak Function ###############################################
 def speak(audio):
-    engine = pyttsx3.init()
-    # engine.setProperty()
-    engine.say(audio)
-    engine.runAndWait()
-
+    """
+    This function convert text to 
+    speech using pyttsx3 module.
+    """
+    try:
+        engine = pyttsx3.init()
+        # engine.setProperty()
+        engine.say(audio)
+        engine.runAndWait()
+    except Exception as e:
+        print(e)
 
 ############################################### Take Command ##################################################
 def take_command():
+    """
+    take command takes human voice as input
+    and then convert it into text to perform
+    operation using speech recognition.
+    """
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening.......")
-        r.pause_threshold = .5
-        r.energy_threshold = 7000
+        r.pause_threshold = 2
+        r.energy_threshold = 10000
         audio = r.listen(source)
 
     try:
@@ -147,44 +259,121 @@ def take_command():
         return "none"
     return query
 
-
 ######################################### Who are You ######################################################
 def whoareyou():
-    speak("Hello ")
-    time.sleep(.5)
-    speak("hi i am edith ")
-
-
-def mathOperation ():
-    result = calculator.binaryCalculator()
-    speak(result)
-
+    """
+    This function tells about that who is this 
+    """
+    try:
+        speak("Hello sir")
+        time.sleep(1)
+        speak("i am edith your personal assistant")
+        speak("would you like to know what can i do")
+        query = take_command.lower()
+        if 'yes' in query or 'yeah' in query or 'sure' in query:
+            features()
+    except Exception as e:
+        print(e)
 
 def shutdown():
-    os = platform.system()
-    os = os.lower()
-    if os =='windows':
-        os.system("shutdown /s")
-    elif os =='linux':
-        os.system()
-    elif os =='macos':
-        os.system()
-    else:
-        speak("Sorry but it can only used in windows , linux , macos")
-
+    """
+    First this function check os name using platform
+    module and then shutdown the system.
+    """
+    try:
+        os = platform.system()
+        os = os.lower()
+        if os =='windows':
+            os.system("shutdown /s")
+        elif os =='linux':
+            os.system()
+        elif os =='macos':
+            os.system()
+        else:
+            speak("Sorry but it can only used in windows , linux , macos")
+    except Exception as e:
+        print(e)
 
 def restart():
-    os = platform.system()
-    os = os.lower()
-    if os =='windows':
-        os.system("shutdown/r")
-    elif os =='linux':
-        os.system()
-    elif os =='macos':
-        os.system()
-    else:
-        speak("Sorry but it can only used in windows , linux , macos")
-    
-    
+    """
+    This function works as shutdown function
+    first it checks os name and then restart
+    the system.
+    """
+    try:
+        os = platform.system()
+
+        os = os.lower()
+        
+        if os =='windows':
+            os.system("shutdown/r")
+        elif os =='linux':
+            os.system()
+        elif os =='macos':
+            os.system()
+        else:
+            speak("Sorry but it can only used in windows , linux , macos")
+    except Exception as e:
+        print(e)
+
+def open_website(query):
+    """
+    This function open any website this 
+    function uses search function which is 
+    in the google search module.
+    """
+    try:
+        for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+            webbrowser.open(j)
+    except Exception as e:
+        print(e)
+
+def features():
+    try:
+        speak("hello sir, i can perform many tasks such as calculating numbers or opening a website for you i can play music for you")
+        speak("I can automate some task for you ")
+    except Exception as e:
+        print(e)
+
+def makeAnote():
+    try:
+        if os.path.isdir("notes"):
+            lst = os.listdir("notes")
+            lstLength = int(len(lst))
+            file_path = f"notes\\note{lstLength+1}.txt"
+            with open (file_path,"a") as file:
+                def write_note():
+                    speak("Okk sir tell me what to write")
+                    # query = input("Enter query :")
+                    query = take_command()
+                    file.write(query)
+                    # print("Want to add more sir :")
+                    speak("Want to add more sir :")
+                    # query = input("More ??")
+                    query = take_command()
+                    if query in ['yes','yeah','hmm','sure']:
+                        write_note()
+                    else:
+                        print("Done sir")
+                        speak("saving note sir")
+                write_note()
+        else:
+            os.makedirs("notes")
+            makeAnote()
+    except Exception as e:
+        print(e)
+
+def myBirthday():
+    try:
+        root = tk.Tk()
+        image1 = Image.open("resources\\images\\birthdayImg.png")
+        test = ImageTk.PhotoImage(image1)
+        label1 = tk.Label(image=test)
+        label1.pack()
+        root.after(10000,lambda:root.destroy())
+        root.mainloop()
+    except Exception as e:
+        print(e)
+
 if __name__ == '__main__':
     pass
--- jarvisMain.py
+++ jarvisMain.py
@@ -1,4 +1,12 @@
-from home import *
+try:
+    from home import *
+    from calculator import *
+    from conversion import *
+    # from alarm import *
+    from threading import Thread
+    from appData import openApp,setting_page
+except Exception as e:
+    print(e)
 """
 greet
 calculate
@@ -12,6 +20,11 @@ operating system
 shutdown
 restart
 battery percentage
+joke
+today's date
+today's day
+setting alarm
+convert temeperature
 """
 ################################################## Main function #############################################
 
@@ -22,47 +35,104 @@ if __name__ == "__main__":
     # weather_data("gorakhpur")
     while True:
         # take_command()
-        query = take_command().lower()
+        # query = take_command().lower()
+        
+        query = input("Enter query :").lower()
+        query = query.replace(":"," ")
+        query = query.replace(".","")
         print(query)
 
-        # if 'wikipedia' in query :
-        #     speak('searching wikipedia.....')
-        #     query = query.replace("wikipedia","")
-        #     results = wikipedia.summary(query, sentence=2)
-        #     speak(results)
+        if 'wikipedia' in query :
+            speak('searching wikipedia.....')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentence=2)
+            speak(results)
 
-        if 'google' in query:
-            # chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-            # webbrowser.get("google-chrome")
-            # webbrowser.open("google.com")
-            webbrowser.open_new_tab("google.com")
+        elif 'greet me' in query or 'wish me' in query:
+            greet()
 
-        elif 'calculate' in query :
+        elif 'my birthday' in query:
+            myBirthday()
 
-            while True:
-                mathOperation()
+        elif 'exit' in query or 'bye' in query or 'goodbye' in query :
+            quit()
 
-        elif 'chrome' in query:
-            # codepath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-            # os.startfile(codepath)
-            os.startfile("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+        elif 'add path' in query or 'add application' in query or 'setting' in query:
+            setting_page()
+            
+        # elif 'set alarm' in query and 'am' in query:
+        #     t1 = Thread(target=am,args=(query,))
+        #     t1.start()
+        #     t1.join()
 
-        # elif 'open web browser' in query :
+        # elif 'set alarm' in query and 'pm' in query:
+
+        #     t2 = Thread(target=pm,args=(query,))
+        #     t2.start()
+        #     t2.join()
+
+        # elif 'set alarm' in query:
+
+        #     t3 = Thread(target=set_alarm_24_hour_clock,args=(query,))
+        #     t3.start()
+        #     t3.join()
+
+        elif 'open' in query:
+            print("reached here")
+            # create_table()
+            openApp(query)
+
+        elif 'subtract' in query or 'minus' in query or '-' in query :
+            subtract_num(query)
+
+        elif 'multiply' in query or 'into' in query or 'x' in query or 'multiplied' in query :
+            multiply_num(query)
+        
+        elif 'add' in query or 'plus' in query or '+' in query :
+            add_num(query)
+        
+        elif 'devide' in query or 'by' in query or '/' in query:
+            devide_num(query)
+        
+        elif 'reminder' in query :
+            reminder(query)
+
+        elif 'square root' in query:
+            sqrt(query)
+    
+        elif 'square' in query :
+            squre(query)
+        
+        elif 'cube root' in query :
+            cube_root(query) 
+        
+        elif 'cube' in query :
+            cube(query)
+   
+        elif 'power' in query :
+            power(query)
+
+        elif 'fahrenhite to Celsius' in query:
+            fahrenhite_to_Celsius(query)
+
+        elif 'celsius to fahrenheit' in query:
+            Celsius_to_fahrenhite(query)  
 
         elif 'news' in query:
             news()
 
         elif 'current time' in query or 'time' in query:
-            time()
+            current_time()
             
         elif 'youtube' in query:
             speak("opening Youtube")
             webbrowser.open("youtube.com")
-            
+
+        elif 'battery percentage' in query :
+            batteryPercantage()
 
         elif 'who are you' in query:
             whoareyou()
-
 
         elif 'weather' in query:
             weather_data()
@@ -73,11 +143,23 @@ if __name__ == "__main__":
         elif 'take a screenshot' in query or 'screenshot' in query:
             screenshot()    
 
-        elif 'operating system' in query:
+        elif 'operating system' in query or 'os' in query:
             osName()
 
-        elif 'exit' in query:
-            quit()
+        elif 'current date' in query or 'today\'s date ' in query or 'date' in query:
+            currentDate()
+
+        elif 'current day' in query or 'today\'s day ' in query or 'day' in query:
+            currentDay()    
+        
+        # elif 'open' in query:
+        #     open_website(query)
+
+        elif 'what task' in query or 'features' in query or 'feature' in query:
+            features()
+
+        elif 'make a note' in query or 'create a note' in query:
+            makeAnote()
 
         else:
             speak("wrong choice")
